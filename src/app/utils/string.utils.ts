06ccd80505dbd9b11fa2fb38d8bf9b0a4c3695b6
Mateusz Nowak <mateusznowak@vp.pl>
2017-10-14 21:58:32 +0200

    Fixed filtering issue

--- src/app/utils/string.utils.ts
+++ src/app/utils/string.utils.ts
@@ -4,6 +4,10 @@ export class StringUtils {
     return !s;
   }
 
+  static isNotEmpty(s: string): boolean {
+    return !this.isEmpty(s);
+  }
+
   static isBlank(s: string): boolean {
     return this.isEmpty(s) || s.trim().length === 0;
   }
@@ -12,12 +16,8 @@ export class StringUtils {
     return !this.isBlank(s);
   }
 
-  static contains(str: string, substr: string): boolean {
-    return str.includes(substr);
-  }
-
   static containsIgnoreCase(str: string, substr: string): boolean {
-    return this.contains(str.toLowerCase(), substr.toLowerCase());
+    return this.isNotEmpty(str) && this.isNotEmpty(substr) && str.toLowerCase().includes(substr.toLowerCase());
   }
 
   static getStringOrNull(s: string): string {
